61e165dad75d007cf5e18a3bca2bdaa1697ff9cc
CHRISHA1122 <chrisha1122@outlook.com>
2023-03-23 11:05:08 +0000

    Added categories

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialaization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
