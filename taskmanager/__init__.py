842f487a0c8304146876ace10f376e841d84bf1e
CHRISHA1122 <chrisha1122@outlook.com>
2023-03-28 12:20:45 +0000

    Bug fix

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -16,6 +16,5 @@ else:
         uri = uri.replace("postgres://", "postgresql://", 1)
         app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
